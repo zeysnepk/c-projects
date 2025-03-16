359ef09e60f33eceefc6e8e25a92342183dedd0c
Zeynep Kaplan <146364601+zeysnepk@users.noreply.github.com>
2025-03-16 16:20:19 +0300

    Update boy.c

--- boy.c
+++ boy.c
@@ -1,44 +1,31 @@
 #include<stdio.h>
+#include<stdlib.h>
 
+/*
+    0 dan büyük öğrenci numaraları için 
+    150 cm boyuna en yakın boy ve noyu basar.
+*/
+
+#define BOY     150
 
 int main()
 {
-    int no,boy,yakin,minboy,minno;
-
-    printf("Ogrenci no: ");
-    scanf("%d",&no);
-
-    printf("Ogrenci boy: ");
-    scanf("%d",&boy);
-
-    yakin=boy-150;
-
-
-
-    while (no>0)
+    int no=0,boy,yakin=__UINT8_MAX__,minboy=0,minno=0;
+    while(1)
     {
         printf("\nOgrenci no: ");
         scanf("%d",&no);
 
-        printf("Ogrenci boy: ");
-        scanf("%d",&boy);
-
-        if(boy>150 && no>0)
-        {
-            if(boy-150<yakin)
-            minboy=boy;
-            minno=no;
+        if(no<=0) break;
+        else{
+            printf("Ogrenci boy: ");
+            scanf("%d",&boy);
+            if(abs(boy-BOY) < yakin){
+                yakin = abs(boy-BOY);
+                minboy=boy;
+                minno=no;
+            }
         }
-
-        else if(boy<=150 && no>0)
-        {
-            if(150-boy<yakin)
-            minboy=boy;
-            minno=no;
-        }
-
-    }
-
+    }    
     printf("%d numarali ogrenci %d cm boyuyla 150 cm ye en yakindir",minno,minboy);
-    
 }
