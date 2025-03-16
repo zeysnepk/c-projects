0d1d578b3ec5379a1996699bb850fd615d4745a4
Zeynep Kaplan <146364601+zeysnepk@users.noreply.github.com>
2025-03-16 15:02:02 +0300

    Update asal.c

--- asal.c
+++ asal.c
@@ -2,7 +2,7 @@
 int main()
 {
     int i, num, count;
-   printf("1 - 10 Arasi Asal Sayilar:\n");
+   printf("1 - 1000 Arasi Asal Sayilar:\n");
    for(num = 1;num<=1000;num++){
       count = 0;
       for(i=2;i<=5;i++){
